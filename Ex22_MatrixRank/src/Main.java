6e1cd74e2206268f7f5d48049816ef52398f63be
stefan <unabledispute@gmail.com>
2015-04-02 15:28:10 +0200

    Methode to get row and col with maximum value

--- Ex22_MatrixRank/src/Main.java
+++ Ex22_MatrixRank/src/Main.java
@@ -32,11 +32,83 @@ public class Main {
 
         System.out.println(unit3x5.toString(0));
 
-        Matrix parse1 = Matrix.parseMatrixFile("/home/hoelk/Dropbox/workspace/java/eprog/Ex22_MatrixRank/src/test_1");
-        Matrix parse2 = Matrix.parseMatrixFile("/home/hoelk/Dropbox/workspace/java/eprog/Ex22_MatrixRank/src/test_2");
+        Matrix test1 = Matrix.parseMatrixFile("/home/hoelk/Dropbox/workspace/java/eprog/Ex22_MatrixRank/src/test_1");
+        Matrix test2 = Matrix.parseMatrixFile("/home/hoelk/Dropbox/workspace/java/eprog/Ex22_MatrixRank/src/test_2");
+        Matrix test3 = Matrix.parseMatrixFile("/home/hoelk/Dropbox/workspace/java/eprog/Ex22_MatrixRank/src/test_3");
+        Matrix test4 = Matrix.parseMatrixFile("/home/hoelk/Dropbox/workspace/java/eprog/Ex22_MatrixRank/src/test_4");
 
-        System.out.println(parse1.toString());
-        System.out.println(parse2.toString());
+        System.out.println("-- test1");
+        System.out.println(test1.toString());
+        System.out.println("Max Row: " + test1.getPosRowWithMax());
+        System.out.println("Max Col: " + test1.getPosColWithMax());
+        System.out.println("");
+        System.out.println("Row Max: " + test1.getRowMax(0) + " at " + test1.getPosRowMax(0));
+        System.out.println("Row Max: " + test1.getRowMax(1) + " at " + test1.getPosRowMax(1));
+        System.out.println("Row Max: " + test1.getRowMax(2) + " at " + test1.getPosRowMax(2));
+        System.out.println("Row Max: " + test1.getRowMax(3) + " at " + test1.getPosRowMax(3));
+        System.out.println("");
+        System.out.println("Col Max: " + test1.getColMax(0) + " at " + test1.getPosColMax(0));
+        System.out.println("Col Max: " + test1.getColMax(1) + " at " + test1.getPosColMax(1));
+        System.out.println("Col Max: " + test1.getColMax(2) + " at " + test1.getPosColMax(2));
+        System.out.println("Col Max: " + test1.getColMax(3) + " at " + test1.getPosColMax(3));
+        System.out.println("Col Max: " + test1.getColMax(4) + " at " + test1.getPosColMax(4));
+
+        System.out.println("-- test2");
+        System.out.println(test2.toString());
+        System.out.println("Max Row: " + test2.getPosRowWithMax());
+        System.out.println("Max Col: " + test2.getPosColWithMax());
+        System.out.println("");
+        System.out.println("Row Max: " + test2.getRowMax(0) + " at " + test2.getPosRowMax(0));
+        System.out.println("Row Max: " + test2.getRowMax(2) + " at " + test2.getPosRowMax(2));
+        System.out.println("Row Max: " + test2.getRowMax(2) + " at " + test2.getPosRowMax(2));
+        System.out.println("");
+        System.out.println("Col Max: " + test2.getColMax(0) + " at " + test2.getPosColMax(0));
+        System.out.println("Col Max: " + test2.getColMax(2) + " at " + test2.getPosColMax(2));
+        System.out.println("Col Max: " + test2.getColMax(2) + " at " + test2.getPosColMax(2));
+
+        System.out.println("-- test3");
+        System.out.println(test3.toString());
+        System.out.println("Max Row: " + test3.getPosRowWithMax());
+        System.out.println("Max Col: " + test3.getPosColWithMax());
+        System.out.println("");
+        System.out.println("Row Max: " + test3.getRowMax(0) + " at " + test3.getPosRowMax(0));
+        System.out.println("Row Max: " + test3.getRowMax(1) + " at " + test3.getPosRowMax(1));
+        System.out.println("Row Max: " + test3.getRowMax(2) + " at " + test3.getPosRowMax(2));
+        System.out.println("");
+        System.out.println("Col Max: " + test3.getColMax(0) + " at " + test3.getPosColMax(0));
+        System.out.println("Col Max: " + test3.getColMax(1) + " at " + test3.getPosColMax(1));
+        System.out.println("Col Max: " + test3.getColMax(2) + " at " + test3.getPosColMax(2));
+        System.out.println("Col Max: " + test3.getColMax(3) + " at " + test3.getPosColMax(3));
+
+
+        System.out.println("-- test4");
+        System.out.println(test4.toString());
+        System.out.println("Max Row: " + test4.getPosRowWithMax());
+        System.out.println("Max Col: " + test4.getPosColWithMax());
+        System.out.println("");
+        System.out.println("Row Max: " + test4.getRowMax(0) + " at " + test4.getPosRowMax(0));
+        System.out.println("Row Max: " + test4.getRowMax(1) + " at " + test4.getPosRowMax(1));
+        System.out.println("Row Max: " + test4.getRowMax(2) + " at " + test4.getPosRowMax(2));
+        System.out.println("");
+        System.out.println("Col Max: " + test4.getColMax(0) + " at " + test4.getPosColMax(0));
+        System.out.println("Col Max: " + test4.getColMax(1) + " at " + test4.getPosColMax(1));
+        System.out.println("Col Max: " + test4.getColMax(2) + " at " + test4.getPosColMax(2));
+        System.out.println("Col Max: " + test4.getColMax(3) + " at " + test4.getPosColMax(3));
+
+
+
+        System.out.println(test2.toString());
+
+        System.out.println(test1.toString());
+        test1.rowSubtract(0, 3);
+        System.out.println(test1.toString());
+
+        test1.rowTimesScalar(1, 2.5);
+        System.out.println(test1.toString());
+
+        test1.rowTimesScalar(3, 40);
+        System.out.println(test1.toString());
+
 
 
     }
--- Ex22_MatrixRank/src/Matrix.java
+++ Ex22_MatrixRank/src/Matrix.java
@@ -1,6 +1,7 @@
 /**
  * Created by Stefan Fleck on 19.03.15.
  */
+
 import java.io.*;
 
 
@@ -8,7 +9,6 @@ public class Matrix {
     public static int numberOfMatrices;
     private final int id;
     private double[][] A;  // [row][col]
-
 
     // Custom constructor
     Matrix(int rows, int cols) {
@@ -40,6 +40,14 @@ public class Matrix {
         }
     }
 
+    // Copy constructor
+
+    Matrix(Matrix B) {
+        A = B.A;
+        id = ++numberOfMatrices;
+    }
+
+
     public static Matrix parseMatrixFile(String path) throws IOException {
 
         FileInputStream istream = new FileInputStream(path);
@@ -61,7 +69,7 @@ public class Matrix {
             elem = line.split(" ");
 
             for (int col = 0; col < cols; col++) {
-               res.setElement(row, col, Double.parseDouble(elem[col]));
+                res.setElement(row, col, Double.parseDouble(elem[col]));
             }
         }
 
@@ -71,11 +79,9 @@ public class Matrix {
 
     }
 
-
     public void setElement(int row, int col, double value) {
-        A[row][col]=value;
+        A[row][col] = value;
     }
-
 
     public void setRow(int row, double[] new_row) {
 
@@ -109,7 +115,86 @@ public class Matrix {
         return (A[0].length);
     }
 
-    private int getId() {
+    public double getRowMax(int row) {
+
+        double max = A[row][0];
+        for (double el : A[row]) {
+            max = Math.max(max, el);
+        }
+        return (max);
+    }
+
+    public double getColMax(int col) {
+
+        double max = A[0][col];
+        for (double[] row : A) {
+            max = Math.max(max, row[col]);
+        }
+        return (max);
+    }
+
+    public int getPosRowMax(int row) {
+        double max = A[row][0];
+        int maxpos = 0;
+
+        for (int i = 0; i < A[0].length; i++) {
+            double newmax = Math.max(max, A[row][i]);
+
+            if (newmax > max) {
+                maxpos = i;
+                max = newmax;
+            }
+        }
+        return (maxpos);
+    }
+
+    public int getPosColMax(int col) {
+
+        double max = A[0][col];
+        int maxpos = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            double newmax = Math.max(max, A[i][col]);
+
+            if (newmax > max) {
+                maxpos = i;
+                max = newmax;
+            }
+        }
+
+        return (maxpos);
+    }
+
+    public int getPosColWithMax() {
+
+        double max = getColMax(0);
+        int maxcol = 0;
+
+        for (int col = 0; col < A[0].length; col++) {
+            double newmax = Math.max(getColMax(col), max);
+            if (newmax > max) {
+                maxcol = col;
+                max = newmax;
+            }
+        }
+        return maxcol;
+    }
+
+    public int getPosRowWithMax() {
+        double max = getRowMax(0);
+        int maxrow = 0;
+
+        for (int row = 0; row < A.length; row++) {
+            double newmax = Math.max(getRowMax(row), max);
+            if (newmax > max) {
+                maxrow = row;
+                max = newmax;
+            }
+        }
+        return maxrow;
+    }
+
+    public int getId() {
         return (id);
     }
 
@@ -131,8 +216,26 @@ public class Matrix {
         }
     }
 
-    public Matrix toTriangularMatrix(Matrix A) {
-        return (A);
+    public void rowSubtract(int row1, int row2) {
+
+        for (int i = 0; i < A[0].length; i++) {
+            A[row1][i] = A[row1][i] - A[row2][i];
+        }
+    }
+
+    public void rowTimesScalar(int row, double scalar) {
+        for (int i = 0; i < A[0].length; i++) {
+            A[row][i] = A[row][i] * scalar;
+        }
+    }
+
+    public Matrix makeTriangular(Matrix X) {
+        Matrix B = new Matrix(X);
+
+
+        return (B);
+
+
     }
 
     public String toString() {
