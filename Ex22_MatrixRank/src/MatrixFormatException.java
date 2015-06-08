9b9f716575adf912c563d15f031ec74619a84345
stefan <unabledispute@gmail.com>
2015-06-08 10:19:28 +0200

    Start Ex 3

--- Ex22_MatrixRank/src/MatrixFormatException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by hoelk on 06.05.15.
- */
-public class MatrixFormatException extends Exception {
-    MatrixFormatException(String message){
-        super(message);
-    }
-}
--- Ex22_MatrixRank/src/MatrixIndexException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by hoelk on 06.05.15.
- */
-public class MatrixIndexException extends Exception {
-    MatrixIndexException(String message){
-            super(message);
-        }
-    }
