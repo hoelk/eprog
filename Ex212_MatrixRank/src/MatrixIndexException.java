0d3bd69d8c3daf8a8d31149bb61bb26b84266a2a
stefan <unabledispute@gmail.com>
2015-06-08 10:20:32 +0200

    Start Ex 3

---
+++ Ex212_MatrixRank/src/MatrixIndexException.java
@@ -0,0 +1,8 @@
+/**
+ * Created by hoelk on 06.05.15.
+ */
+public class MatrixIndexException extends Exception {
+    MatrixIndexException(String message){
+            super(message);
+        }
+    }
