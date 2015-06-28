b910a6aa84c4efe3e784ee78169473c8993d21c2
stefan <unabledispute@gmail.com>
2015-06-28 18:06:13 +0200

    Implemented equals and compareTo

---
+++ ex22_collections/src/Place.java
@@ -0,0 +1,84 @@
+/**
+ * Created by hoelk on 23.06.15.
+ */
+
+public class Place implements Comparable<Place> {
+    String countryCode;
+    String cityNameASCII;
+    String cityName;
+    String region;
+    int population;
+    double lat;
+    double lon;
+
+
+    Place(String countryCode, String cityNameASCII, String cityName, String region, int population, double lat, double lon) {
+
+        try {
+            checkPlace(countryCode, cityNameASCII, cityName, region, population, lat, lon);
+        } catch (InvalidInputException e) {
+            System.err.println("\nCaught InvalidInputException: " + e.getMessage());
+        }
+
+        this.countryCode = countryCode;
+        this.cityNameASCII = cityNameASCII;
+        this.cityName = cityName;
+        this.region = region;
+        this.population = population;
+        this.lat = lat;
+        this.lon = lon;
+
+    }
+
+    void checkPlace(String countryCode, String cityNameASCII, String cityName, String region, int population, double lat, double lon) throws InvalidInputException {
+
+        if (countryCode.length() != 2) throw new InvalidInputException("Country Code not valid");
+        if (cityNameASCII.length() > 100) throw new InvalidInputException("Ascii City Name not valid");
+        if (cityName.length() > 255) throw new InvalidInputException("City Name not valid");
+        if (region.length() != 2) throw new InvalidInputException("region not valid");
+        if (population < -1) throw new InvalidInputException("population not valid");
+        if (lat > 90 || lat < -90) throw new InvalidInputException("lat not valid");
+        if (lon > 180 || lon < -180) throw new InvalidInputException("lon not valid");
+
+    }
+
+
+    public int compareTo(Place other) {
+        if (other.population > this.population) {
+            return 1;
+        }
+        if (other.population < this.population) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+
+    @Override
+    public boolean equals(Place other) {
+        return (other.countryCode.equals(countryCode) &&
+                other.cityNameASCII.equals(cityNameASCII) &&
+                other.cityName.equals(cityName) &&
+                other.region.equals(region) &&
+                other.population == population &&
+                other.lat == lat &&
+                other.lon == lon);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return population;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %30s %30s %5s %10d %10.3f %10.3f", countryCode, cityNameASCII, cityName, region, population, lat, lon);
+    }
+
+    /* Implement Equals
+ /* do i also need to implement a hash function? */
+
+
+}
