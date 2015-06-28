18cba5289e1754cc88eb6d1f8ac5c860575df7d6
stefan <unabledispute@gmail.com>
2015-06-28 18:04:57 +0200

    Trying out hash tables

--- ex22_collections/src/Places.java
+++ ex22_collections/src/Places.java
@@ -1,25 +1,34 @@
 import java.util.ArrayList;
 import java.io.*;
+import java.util.HashSet;
 
 /**
  * Created by hoelk on 25.06.15.
  */
 public class Places {
 
-    ArrayList<Place> Places = new ArrayList<Place>();
+    HashSet<Place> Places = new HashSet<Place>();
+
+    Places(String path) {
+
+        try {
+            Places = readCsvPlaces(path);
+        } catch (IOException e) {
+            System.err.println("\nWarning: IOException: " + e.getMessage());
+        }
+    }
 
 
-    public static void readCsv(String path) throws IOException {
+    public static HashSet<Place> readCsvPlaces(String path) throws IOException {
         System.out.println("Reading csv file");
 
         FileInputStream istream = new FileInputStream(path);
         InputStreamReader reader = new InputStreamReader(istream);
         BufferedReader input = new BufferedReader(reader);
-
-        String line = input.readLine();
+        HashSet<Place> Places = new HashSet<Place>();
 
         for (int row = 1; row < 1000; row++) {
-            line = input.readLine();
+            String line = input.readLine();
             String[] elem = line.split(",");
 
             String CountryCode = elem[0];
@@ -31,26 +40,21 @@ public class Places {
             try {
                 Population = Integer.parseInt(elem[4]);
             } catch (NumberFormatException e) {
-              //  System.err.println("\nWarning: NumberFormatException when parsing Population: " + e.getMessage());
+                //  System.err.println("\nWarning: NumberFormatException when parsing population: " + e.getMessage());
             }
             double Latitude = Double.parseDouble(elem[5]);
             double Longitude = Double.parseDouble(elem[6]);
 
-            Place temp = new Place(CountryCode, CityNameASCII, CityName, Region, Population,  Latitude, Longitude);
+            Place temp = new Place(CountryCode, CityNameASCII, CityName, Region, Population, Latitude, Longitude);
+            Places.add(temp);
+
             System.out.println(temp.toString());
-
-
-
-
-
-
         }
 
         input.close();
+        return Places;
 
     }
-
-
 }
 
 
